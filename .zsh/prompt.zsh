ff584863007eb4de56082177b050b181675dcb93
Caio Filipini <caiofilipini@gmail.com>
2014-01-30 22:10:05 +0100

    Change RPROMPT color back to yellow.

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -3,19 +3,19 @@ setopt prompt_subst
 RESET_COLOR="%f%b"
 
 function __ruby_version {
-  echo "$(chruby | grep '*' | tr -d '* ')"
+        echo "%F{yellow}$(chruby | grep '*' | tr -d '* ')$RESET_COLOR"
 }
 
 function __git_status() {
-	STATUS=""
-	if [[ -n $(git status -s 2> /dev/null) ]]; then
-		STATUS=" %F{yellow}✗$RESET_COLOR"
-	fi
-	INDEX=$(git status --porcelain 2> /dev/null)
-	if $(echo "$INDEX" | grep '^?? ' &> /dev/null); then
-		STATUS="$STATUS%F{blue} ?$RESET_COLOR"
-	fi
-	echo $STATUS
+        STATUS=""
+        if [[ -n $(git status -s 2> /dev/null) ]]; then
+                STATUS=" %F{yellow}✗$RESET_COLOR"
+        fi
+        INDEX=$(git status --porcelain 2> /dev/null)
+        if $(echo "$INDEX" | grep '^?? ' &> /dev/null); then
+                STATUS="$STATUS%F{blue} ?$RESET_COLOR"
+        fi
+        echo $STATUS
 }
 
 function __git_branch() {
@@ -24,9 +24,9 @@ function __git_branch() {
                 ref=$(git reflog 2> /dev/null | grep checkout | head -n1 | awk '{print $NF}')
         fi
         if [ -z "$ref" ]; then
-		echo " "
-	else
-		echo " %F{blue}git:(%F{red}$ref%F{blue})%f%b$(__git_status) "
+                echo " "
+        else
+                echo " %F{blue}git:(%F{red}$ref%F{blue})$RESET_COLOR$(__git_status) "
         fi
 }
 
